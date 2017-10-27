928426d358147de82576b81b9ca1ff6b5d74a604
Jay Lara <jay.lara@outlook.com>
2017-10-26 21:42:36 -0500

    HW_W07D04 Jay initial commit

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      balance: 0
+    }
+
+    //adds amount to balance, only if amount is a valid number
+    this._addAmount = (e) => {
+      var parsedValue = parseFloat(this.refs.amount.value);
+      if(!Number.isNaN(parsedValue)) {
+        this.setState({balance: this.state.balance + parsedValue})
+      } // end of if
+      this.refs.amount.value = "";
+    } // end of _addAmount()
+
+
+    //subtracts amount from balance, only if amount is a valid number
+    // and amount is less than or equal to balance
+    this._subtractAmount = (e) => {
+      var parsedValue = parseFloat(this.refs.amount.value);
+      if(!Number.isNaN(parsedValue) && parsedValue <= this.state.balance) {
+        this.setState({balance: this.state.balance - parsedValue})
+      } // end of if
+      this.refs.amount.value = "";
+    } // end of _addAmount()
+  }
   render() {
+    let balanceClass = 'balance';
+    
+    if(this.state.balance === 0)
+      balanceClass += ' zero';
+
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={balanceClass}>{this.state.balance.toFixed(2)}</div>
+        <input type="text" placeholder="enter an amount" ref="amount" />
+        <input type="button" value="Deposit" onClick={this._addAmount}/>
+        <input type="button" value="Withdraw" onClick={this._subtractAmount}/>
       </div>
     )
   }
