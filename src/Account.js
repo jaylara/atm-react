8c921e32387a31f9da919adccaceaed4a35964db
Jay Lara <jay.lara@outlook.com>
2017-10-26 23:03:11 -0500

    HW_W07D04 Jay added transaction history 4 da lolz

--- src/Account.js
+++ src/Account.js
@@ -11,27 +11,37 @@ export default class Account extends Component {
     this._addAmount = (e) => {
       var parsedValue = parseFloat(this.refs.amount.value);
       if(!Number.isNaN(parsedValue)) {
-        this.setState({balance: this.state.balance + parsedValue})
+        this.setState({balance: this.state.balance + parsedValue});
+        this.sendTransactionUpdate(this.props.name, "Added", parsedValue);
       } // end of if
       this.refs.amount.value = "";
     } // end of _addAmount()
-
 
     //subtracts amount from balance, only if amount is a valid number
     // and amount is less than or equal to balance
     this._subtractAmount = (e) => {
       var parsedValue = parseFloat(this.refs.amount.value);
       if(!Number.isNaN(parsedValue) && parsedValue <= this.state.balance) {
-        this.setState({balance: this.state.balance - parsedValue})
+        this.setState({balance: this.state.balance - parsedValue});
+        this.sendTransactionUpdate(this.props.name, "Removed", parsedValue);
       } // end of if
       this.refs.amount.value = "";
-    } // end of _addAmount()
-  }
+    } // end of _subtractAmount()
+
+
+  }//end of constructor
+
+  //send a transaction message to the parent that the account has been updated.
+  sendTransactionUpdate = (accountName, action, value) => {
+    this.props.onUpdate(`${accountName}: ${action} $${value}`);
+  } // end of sendTransactionUpdate()
+
   render() {
     let balanceClass = 'balance';
-    
-    if(this.state.balance === 0)
-      balanceClass += ' zero';
+
+    if(this.state.balance === 0) {
+        balanceClass += ' zero';
+    } // end of if
 
     return (
       <div className="account">
--- src/App.js
+++ src/App.js
@@ -4,19 +4,45 @@ import logo from './ga.png';
 import Account from './Account';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      transactionHistory: []
+    }
+  } // end of constructor
+
+  //update transaction history array with message from Account
+  updateTransactionHistory = (message) => {
+    var newTH = this.state.transactionHistory;
+    newTH.push({message});
+    this.setState({transactionHistory : newTH});
+  } // end of updateTransactionHistory()
+
   render() {
+    let transactionHistory = "";
+    //render transaction history only is transaction history is valid
+    if(this.state.transactionHistory)
+      transactionHistory = this.state.transactionHistory.map( (trans, idx) => (
+        <li key={idx}>{trans.message}</li>
+      ));
     return (
       <div id="content">
         <div id="nav">
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account name={'Checking'}/>
-        <Account name={'Savings'}/>
-        <div className="clear"></div>
+        <Account name={'Checking'} onUpdate={this.updateTransactionHistory}/>
+        <Account name={'Savings'} onUpdate={this.updateTransactionHistory}/>
+
+        {/*TODO: Consider placing this area in TransactionHistory Component later */}
+        <div className="transaction-history">
+          <h2>Transaction History</h2>
+          <ul>{transactionHistory}</ul>
+          <div className="clear"></div>
+        </div>
       </div>
     );
-  }
-}
+  } // end of render()
+} // end of class App
 
 export default App;
